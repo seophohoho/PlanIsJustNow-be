579d841ed4c169528c6515e657e78db6ca65bdcf
seophohoho <seop0937@gmail.com>
2024-01-03 13:56:01 +0900

    Initial enviroment settings

---
+++ src/main/java/com/planisjustnow/PlanisjustnowApplication.java
@@ -0,0 +1,13 @@
+package com.planisjustnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanisjustnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanisjustnowApplication.class, args);
+	}
+
+}
