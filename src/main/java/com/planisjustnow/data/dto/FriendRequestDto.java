508457f1e6f16f221ce93b851dc00cdc46f4c85b
silverclean091 <wjdghkdidi@naver.com>
2024-04-03 03:00:24 +0900

    [feat:friend-request] Add FriendRequestDto file

---
+++ src/main/java/com/planisjustnow/data/dto/FriendRequestDto.java
@@ -0,0 +1,15 @@
+package com.planisjustnow.data.dto;
+
+public class FriendRequestDto {
+
+    String from;
+    String to;
+
+    public String getFrom() { return from; }
+
+    public void setFrom(String from) { this.from = from; }
+
+    public String getTo() { return to; }
+
+    public void setTo(String to) { this.to = to; }
+}
