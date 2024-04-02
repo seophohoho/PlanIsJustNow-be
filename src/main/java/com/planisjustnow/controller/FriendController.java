2325cebc3f0542867ddc7938aaf66002aca421da
silverclean091 <wjdghkdidi@naver.com>
2024-04-03 02:59:02 +0900

    [feat:friend] Edit FriendController file

--- src/main/java/com/planisjustnow/controller/FriendController.java
+++ src/main/java/com/planisjustnow/controller/FriendController.java
@@ -1,4 +1,29 @@
 package com.planisjustnow.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/friend")
 public class FriendController {
+
+    // @Autowired
+
+    // 친구 요청
+    //@PostMapping("friend-request");
+
+    // 친구 수락
+    //@PostMapping("friend-acceptance");
+
+    // 친구 거절
+    //@PostMapping("friend-refusal");
+
+    // 친구 조회
+    //@PostMapping("friend-inquiry");
+
+    // 친구 삭제
+    //@PostMapping("friend-delete");
+
 }
