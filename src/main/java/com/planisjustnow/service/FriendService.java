fe29cb3fbc881384bb89a13e85b7f27931d21870
silverclean091 <wjdghkdidi@naver.com>
2024-04-08 03:21:45 +0900

    [feat:friendRequest] Create friendRequest method (Modifying)

--- src/main/java/com/planisjustnow/service/FriendService.java
+++ src/main/java/com/planisjustnow/service/FriendService.java
@@ -1,6 +1,8 @@
 package com.planisjustnow.service;
 
+import com.planisjustnow.data.dto.FriendDto;
 import com.planisjustnow.data.repository.FriendRepository;
+import com.planisjustnow.data.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -8,5 +10,12 @@ public class FriendService {
 
     @Autowired
     private FriendRepository friendRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public String friendRequest(FriendDto friendDto) {
+
+    }
 
 }
