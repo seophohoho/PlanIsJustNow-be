3e7da43da22451c7e1666a23782801e8dbe189ee
silverclean091 <wjdghkdidi@naver.com>
2024-04-14 01:57:04 +0900

    [feat:friendRequest] Create friendRequest method

--- src/main/java/com/planisjustnow/service/FriendService.java
+++ src/main/java/com/planisjustnow/service/FriendService.java
@@ -2,10 +2,13 @@ package com.planisjustnow.service;
 
 import com.planisjustnow.data.dto.FriendDto;
 import com.planisjustnow.data.entity.FriendEntity;
+import com.planisjustnow.data.entity.UserEntity;
 import com.planisjustnow.data.repository.FriendRepository;
 import com.planisjustnow.data.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
 
 public class FriendService {
 
@@ -22,10 +25,21 @@ public class FriendService {
         String toUser = friendDto.getTo();
 
         // 요청 정보를 기반으로 새로운 친구 요청 Entity 생성하기
-        FriendEntity friendRequest = new FriendEntity();
-        friendRequest.setFrom(userRepository.findById(fromUser).orElse(null));
-        friendRequest.setTo(userRepository.findById(toUser).orElse(null));
-        friendRequest.setIs_friend(0);
+        // (from - to - isFriend)
+        FriendEntity friendRequest1 = new FriendEntity();
+        friendRequest1.setFrom(userRepository.findById(fromUser).orElse(null));
+        friendRequest1.setTo(userRepository.findById(toUser).orElse(null));
+        friendRequest1.setIs_friend(0);
+        // (from - to - isFriend)
+        FriendEntity friendRequest2 = new FriendEntity();
+        friendRequest2.setTo(userRepository.findById(fromUser).orElse(null));
+        friendRequest2.setFrom(userRepository.findById(toUser).orElse(null));
+        friendRequest2.setIs_friend(0);
+
+        // 데이터베이스에 Entity 저장
+        friendRepository.saveAll(Arrays.asList(friendRequest1, friendRequest2));
+
+        return "친구 요청이 완료되었습니다.";
     }
 
 }
