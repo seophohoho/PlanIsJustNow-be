12be3c7900f1d4783fe480042d9f4d682123f15a
silverclean091 <wjdghkdidi@naver.com>
2024-04-15 00:41:33 +0900

    [feat:friendRequest] Create sendFriendRequest Service

--- src/main/java/com/planisjustnow/controller/FriendController.java
+++ src/main/java/com/planisjustnow/controller/FriendController.java
@@ -1,9 +1,14 @@
 package com.planisjustnow.controller;
 
+import com.planisjustnow.data.dto.FriendDto;
+import com.planisjustnow.data.dto.ResponseDto;
 import com.planisjustnow.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,7 +19,17 @@ public class FriendController {
     private FriendService friendService;
 
     // 친구 요청
-    //@PostMapping("friend-request");
+    @PostMapping("friendRequest")
+    public ResponseEntity<ResponseDto> sendFriendRequest(@RequestBody FriendDto friendDto) {
+        String result = friendService.friendRequest(friendDto);
+        if (result.equals("success")) {
+            ResponseDto responseDto = new ResponseDto("success", ".");
+            return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
+        } else {
+            ResponseDto responseDto = new ResponseDto("fail", "Unexpected error");
+            return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+        }
+    }
 
     // 친구 수락
     //@PostMapping("friend-acceptance");
