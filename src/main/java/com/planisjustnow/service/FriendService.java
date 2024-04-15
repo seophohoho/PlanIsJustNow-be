d4c8e8342c5e47fc1cfcf31d5c1e915f5da12e8e
silverclean091 <wjdghkdidi@naver.com>
2024-04-16 05:09:19 +0900

    [feat:friendAccept] Create requestAccept Service (Modifying)

--- src/main/java/com/planisjustnow/service/FriendService.java
+++ src/main/java/com/planisjustnow/service/FriendService.java
@@ -19,32 +19,57 @@ public class FriendService {
     private FriendRepository friendRepository;
     @Autowired
     private UserRepository userRepository;
+
     @Transactional
     public String friendRequest(FriendDto friendDto) {
-
-        // 클라이언트로부터 받은 FriendDto에서 요청 정보 추가하기
-
-        // 요청 정보를 기반으로 새로운 친구 요청 Entity 생성하기
-
         try{
             String fromUser = friendDto.getFrom();
             String toUser = friendDto.getTo();
+
+            // 클라이언트로부터 받은 사용자 2명의 정보 등록
             Optional<UserEntity> fromUserEntity = userRepository.findById(fromUser);
             Optional<UserEntity> toUserEntity = userRepository.findById(toUser);
 
+            // 요청 정보를 기반으로 새로운 친구 요청 Entity 생성하기 (from-to)
             FriendEntity friendRequest1 = new FriendEntity();
             friendRequest1.setFrom(fromUserEntity.get());
             friendRequest1.setTo(toUserEntity.get());
             friendRequest1.setIs_friend(0);
             friendRepository.save(friendRequest1);
-
+            // 요청 정보를 기반으로 새로운 친구 요청 Entity 생성하기 (to-from)
             FriendEntity friendRequest2 = new FriendEntity();
             friendRequest2.setFrom(toUserEntity.get());
             friendRequest2.setTo(fromUserEntity.get());
             friendRequest2.setIs_friend(0);
             friendRepository.save(friendRequest2);
             return "success";
+
         } catch (NullPointerException e) {
+            return "fail";
+        }
+    }
+
+    @Transactional
+    public String requestAccept(FriendDto friendDto) {
+        try {
+            String fromUser = friendDto.getFrom();
+            String toUser = friendDto.getTo();
+
+            // 친구 요청을 보낸 사용자와 요청을 받은 사용자의 Entity 확인
+            Optional<UserEntity> fromUserEntity = userRepository.findById(fromUser);
+            Optional<UserEntity> toUserEntity = userRepository.findById(toUser);
+
+            // 사용자 정보가 없을 경우, 요청 실패 처리
+            if (!fromUserEntity.isPresent() || !toUserEntity.isPresent()) {
+                return "fail";
+            }
+
+            // 친구 요청 수락 (is_friend의 값을 1로 변경)
+            Optional<FriendEntity> friendRequest = friendRepository.findByFromAndTo(fromUserEntity.get(), toUserEntity.get());
+
+            return "success";
+
+        } catch (Exception e) {
             return "fail";
         }
     }
